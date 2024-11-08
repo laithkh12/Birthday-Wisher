b2b6412c08e3c196ad876ce4788bcb7c5f96b7a1
laith12khater <laithkhater12@gmail.com>
2024-11-08 22:46:45 +0200

    code added

---
+++ Birthday Wisher/main.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+myEmail = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+
+
+
+today = datetime.now()
+todayTuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdayDict = {(dataRow["month"],dataRow["day"]): dataRow for (index, dataRow) in data.iterrows()}
+
+if todayTuple in birthdayDict:
+    birthdayPerson = birthdayDict[todayTuple]
+    filePath = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(filePath) as letterFile:
+        contents = letterFile.read()
+        contents = contents.replace("[NAME]", birthdayPerson["name"])
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(myEmail, password)
+        connection.sendmail(from_addr=myEmail, to_addrs=birthdayPerson["email"], msg=f"Subject:Happy Birthday!\n\n{contents}")
